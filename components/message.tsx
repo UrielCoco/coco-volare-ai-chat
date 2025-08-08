e228b438fed73640e3a41a6532156f5933d34c2c
UrielCoco <urielcastro.co@gmail.com>
2025-08-07 19:17:17 -0500

    style 19

--- components/message.tsx
+++ components/message.tsx
@@ -98,11 +98,13 @@ const PurePreviewMessage = ({
               if (message.role === 'assistant' && isLoading) {
                 return (
                   <div key={key} className="flex justify-center py-4">
-                    <img src="../public/thinking.gif" alt="Pensando..." className="w-12 h-12 opacity-80" />
+                    Pensando<img src="../thinking.gif" alt="Pensando..." className="w-12 h-12 opacity-80" />
                   </div>
                 );
               }
 
+
+              
               if (type === 'text' && part.text && typeof part.text === 'string') {
                 return (
                   <div key={key} className="flex flex-row gap-2 items-start">
